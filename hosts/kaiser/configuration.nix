7ab8f695a98cf53c2f97e5cd921f38b7171a3fd1
Jan Tojnar <jtojnar@gmail.com>
2017-09-18 15:20:50 +0200

    kaiser: Increase number od fs watches

--- hosts/kaiser/configuration.nix
+++ hosts/kaiser/configuration.nix
@@ -31,6 +31,13 @@ in {
     }
   ];
 
+  boot.kernel.sysctl = {
+    # Note that inotify watches consume 1kB on 64-bit machines.
+    "fs.inotify.max_user_watches"   = 1048576;   # default:  8192
+    "fs.inotify.max_user_instances" =    1024;   # default:   128
+    "fs.inotify.max_queued_events"  =   32768;   # default: 16384
+  };
+
   networking.hostName = "kaiser"; # Define your hostname.
   # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
 
