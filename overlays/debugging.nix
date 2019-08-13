b5bb6ffe93c1b8a825255a8feb8cf24ba44fa21a
Jan Tojnar <jtojnar@gmail.com>
2019-08-13 17:34:35 +0200

    debugging: stop debugging deadbeef & rygel
    reduces rebuilds

--- overlays/debugging.nix
+++ overlays/debugging.nix
@@ -1,18 +1,9 @@
 (self: super: {
-  deadbeef = super.enableDebugging super.deadbeef;
-  deadbeefPlugins = builtins.mapAttrs (name: plugin: super.enableDebugging plugin) super.deadbeefPlugins;
-
   gnome3 = super.gnome3.overrideScope' (gself: gsuper: {
     geary = super.enableDebugging gsuper.geary;
 
     polari = super.enableDebugging (gsuper.polari.override {
       gjs = super.enableDebugging gsuper.gjs;
-    });
-
-    rygel = super.enableDebugging (gsuper.rygel.override {
-      tracker = super.enableDebugging (gsuper.tracker.overrideAttrs (attrs: {
-        mesonBuildType = "debug";
-      }));
     });
   });
 })
