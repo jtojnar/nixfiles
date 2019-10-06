751e8764bc4b7fdfdf966fb7a085651af4a88e19
Jan Tojnar <jtojnar@gmail.com>
2019-10-06 05:29:08 +0200

    azazel: init

---
+++ hosts/azazel/configuration.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+let
+  keys = import ../../keys.nix;
+in {
+  imports = [
+    <nixpkgs/nixos/modules/profiles/minimal.nix>
+    <nixpkgs/nixos/modules/virtualisation/container-config.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+    ./build.nix
+    ./networking.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    vim
+  ];
+
+  programs = {
+    fish.enable = true;
+  };
+
+  services = {
+    openssh = {
+      enable = true;
+      passwordAuthentication = false;
+    };
+  };
+
+  users = {
+    users = {
+      root = {
+        openssh.authorizedKeys.keys = keys.jtojnar;
+        hashedPassword = "*";
+      };
+      jtojnar = {
+        isNormalUser = true;
+        uid = 1000;
+        extraGroups = [ "wheel" ];
+        openssh.authorizedKeys.keys = keys.jtojnar;
+        hashedPassword = "$6$yqXBTritxLsTNhy.$baY8JEagVyeBmpV6WCLY7nH4YH6YAjWiBPAvgF0zcVjYr7yagBmpZtmX/EFMedgxbCnU7l97SdG7EV6yfT.In/";
+      };
+    };
+
+    defaultUserShell = pkgs.fish;
+    mutableUsers = false;
+  };
+
+  time.timeZone = "Europe/Prague";
+
+  documentation.enable = true;
+  documentation.nixos.enable = true;
+
+  system.stateVersion = "18.09";
+}
