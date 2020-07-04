c8285c3b5f2cd2fb0f2f241cf19487bd2923f4fb
Jan Tojnar <jtojnar@gmail.com>
2020-07-04 02:23:43 +0200

    packages.selfoss: 2.19-9f41abb → 2.19-664481d

--- common/pkgs/selfoss/default.nix
+++ common/pkgs/selfoss/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   pname = "selfoss";
-  version = "2.19-9f41abb";
+  version = "2.19-664481d";
 
   src = fetchurl {
     url = "https://dl.bintray.com/fossar/selfoss/selfoss-${version}.zip";
-    sha256 = "xQQQkmBnXjKNpGmRtV59M8kwV853x2kdyounu8Geq0A=";
+    sha256 = "gnGpcCBHjQvdL5GJyU7fDDV0eACJtcnySBnhZ2PiThw=";
   };
 
   nativeBuildInputs = [
