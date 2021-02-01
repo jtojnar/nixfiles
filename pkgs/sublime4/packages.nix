d0099dc079a46c2af916d49ae4e18ea2b935b9e2
Jan Tojnar <jtojnar@gmail.com>
2021-02-01 18:01:27 +0100

    sublime4-dev: 4094 → 4096

--- pkgs/sublime4/packages.nix
+++ pkgs/sublime4/packages.nix
@@ -5,9 +5,9 @@ let
 in
   {
     sublime4-dev = common {
-      buildVersion = "4094";
+      buildVersion = "4096";
       dev = true;
-      x64sha256 = "0i566aqk11hwgm31c0pycr68rp0hhyspc1rynhs6b8gc1j1ql9r6";
-      aarch64sha256 = "0448rg92jb5nf8aaag649pzigrxki63878yx4car4sivapdw3bz0";
+      x64sha256 = "16c6sdaw7n3949i499kpcq9p1qiq29xhdsr21l5rqm241gm09pk5";
+      aarch64sha256 = "0qgi9xcz2pbjzvjfmpa1ni7r9hq8f0zh0hb6slnn73r9mgahq8lc";
     } {};
   }
