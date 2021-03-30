29fa7e8562307b897a80831e2a6897de1f0960f2
Jan Tojnar <jtojnar@gmail.com>
2021-03-30 13:19:14 +0200

    Remove debugging overlay

--- common/overlays/debugging/default.nix
+++
@@ -1,9 +0,0 @@
-final: prev: {
-  gnome3 = prev.gnome3.overrideScope' (gself: gsuper: {
-    geary = prev.enableDebugging gsuper.geary;
-
-    polari = prev.enableDebugging (gsuper.polari.override {
-      gjs = prev.enableDebugging gsuper.gjs;
-    });
-  });
-}
