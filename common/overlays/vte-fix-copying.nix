68189746ad56cc112f21f9894a6b19cb32b416cb
Jan Tojnar <jtojnar@gmail.com>
2021-05-16 19:02:22 +0200

    overlays: Fix copying all text in GNOME Terminal
    For some users, copying text selected using the “Select All” function caused VTE to crash due to running out of memory so upstream decided to change “Select All” to only select the visible text. I had no such problem and I used the feature very often so I have to revert this unfortunate “fix”.
    
    Upstream issue: https://gitlab.gnome.org/GNOME/gnome-terminal/-/issues/288

---
+++ common/overlays/vte-fix-copying.nix
@@ -0,0 +1,13 @@
+final: prev: {
+  vte = prev.vte.overrideAttrs (attrs: {
+    patches = attrs.patches or [] ++ [
+      # Fix copying all terminal text.
+      # https://gitlab.gnome.org/GNOME/gnome-terminal/issues/288
+      (prev.fetchpatch {
+        url = "https://gitlab.gnome.org/GNOME/vte/commit/73713ec0644e232fb740170e399282be778d97f9.patch";
+        sha256 = "xES6Oain9I8A/5uEXws4XJKthwyE64HoInAtAsDs7p0=";
+        revert = true;
+      })
+    ];
+  });
+}
