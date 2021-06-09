3be8b1a0b2d822e14f2e7360594c81416f5ece3e
Jan Tojnar <jtojnar@gmail.com>
2021-06-09 20:05:33 +0200

    default.nix: make it more like nixpkgs
    so that update scripts work more reliably

--- default.nix
+++ default.nix
@@ -1,11 +1,18 @@
-(import (
-  let
-    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  in
-    fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
-    }
-) {
-  src =  ./.;
-}).defaultNix
+# So that this can be imported like nixpkgs by various update scripts.
+{ ... }:
+let
+  self = import (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+      fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+        sha256 = lock.nodes.flake-compat.locked.narHash;
+      }
+  ) {
+    src =  ./.;
+  };
+in
+  # So that various update scripts can find the packages.
+  self.defaultNix.outputs.packages.${builtins.currentSystem}
+  // self.defaultNix
