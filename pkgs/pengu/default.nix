bee6910fdd9c9926e59a65b367f0b3636e7dff17
Jan Tojnar <jtojnar@gmail.com>
2021-07-03 16:53:16 +0200

    pkgs.pengu: clean up

--- pkgs/pengu/default.nix
+++ pkgs/pengu/default.nix
@@ -1,14 +1,11 @@
 {
   runCommand,
   fetchFromGitHub,
-  nodejs_latest,
   napalm,
   unstableGitUpdater,
 }:
 
 let
-  nodejs = nodejs_latest;
-
   stopNpmCallingHome = ''
     # Do not try to find npm in napalm-registry –
     # it is not there and checking will slow down the build.
@@ -24,56 +21,50 @@ let
     rev = "2d547241feed1b2a1b9515c1cd45d6f3d90246ea";
     sha256 = "sha256-UbQs2ga1Kjg+BwSFVdOhOuQz5wlGRHET7XY5fQdoHsg=";
   };
-
-  pengu-deps = napalm.buildPackage src {
-    npmCommands = [
-      # Just download and unpack all the npm packages,
-      # we need napalm to patch shebangs before we can run install scripts.
-      "npm install --loglevel verbose --ignore-scripts"
-    ];
-
-    postConfigure = stopNpmCallingHome;
-
-    postBuild = ''
-      # Patch shebangs so that scripts can run.
-      for f in node_modules/.bin/node-gyp-build node_modules/.bin/parcel; do
-          patchShebangs "$(readlink -f "$f")"
-      done
-
-      # Let’s install again, this time running scripts.
-      npm install --loglevel verbose
-    '';
-  };
 in
-  runCommand "pengu" {
-    version = "unstable-2021-07-03";
+napalm.buildPackage src rec {
+  name = "pengu-${version}";
+  version = "unstable-2021-07-03";
 
-    nativeBuildInputs = [
-      nodejs
-    ];
+  npmCommands = [
+    # Just download and unpack all the npm packages,
+    # we need napalm to patch shebangs before we can run install scripts.
+    "npm install --loglevel verbose --ignore-scripts"
 
-    passthru = {
-      inherit src;
-      updateScript = unstableGitUpdater {
-        # The updater tries src.url by default, which does not exist for fetchFromGitHub (fetchurl).
-        url = "${src.meta.homepage}.git";
-      };
-    };
-  } ''
-    # Required for npm config.
-    export HOME=$(mktemp -d)
+    # Let’s install again, this time running scripts.
+    "npm install --loglevel verbose"
 
-    cp -r ${pengu-deps}/* .
-    chmod +w -R _napalm-install
-    cd _napalm-install
-
-    ${stopNpmCallingHome}
+    # Patch shebangs so that scripts can run.
+    # napalm’s patching is not “overzealous” enough
+    # to fix the file linked by “node_modules/.bin/parcel”.
+    "patchShebangs node_modules/parcel/lib/bin.js"
 
     # Build the front-end.
-    npm run build
-    # Clean up node_modules.
-    npm install --production  --loglevel verbose
+    "npm run build"
+
+    # Clean up node_modules for production.
+    "npm install --production --loglevel verbose"
+  ];
+
+  postConfigure = ''
+    # configurePhase sets $HOME
+    ${stopNpmCallingHome}
+  '';
+
+  installPhase = ''
+    runHook preInstall
 
     mkdir -p $out
     cp -r * $out
-  ''
+
+    runHook postInstall
+  '';
+
+  passthru = {
+    inherit src;
+    updateScript = unstableGitUpdater {
+      # The updater tries src.url by default, which does not exist for fetchFromGitHub (fetchurl).
+      url = "${src.meta.homepage}.git";
+    };
+  };
+}
