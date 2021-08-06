3eca1c33e0cfe8779baedcdcb475b3fb4aa871b0
Jan Tojnar <jtojnar@gmail.com>
2021-08-06 22:10:01 +0200

    theo: add KinT/QMK/Teensy udev rules
    udev rules from https://github.com/NixOS/nixpkgs/pull/130847

--- hosts/theo/configuration.nix
+++ hosts/theo/configuration.nix
@@ -210,6 +210,21 @@ in {
     zotero
   ];
 
+  services.udev.packages = with pkgs; [
+    qmk-udev-rules
+  ];
+
+  services.udev.extraRules = ''
+    # https://www.pjrc.com/teensy/00-teensy.rules
+    ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04*", ENV{ID_MM_DEVICE_IGNORE}="1", ENV{ID_MM_PORT_IGNORE}="1"
+    ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789a]*", ENV{MTP_NO_PROBE}="1"
+    KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04*", MODE:="0666", RUN:="${pkgs.coreutils}/bin/stty -F /dev/%k raw -echo"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04*", MODE:="0666"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04*", MODE:="0666"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="1fc9", ATTRS{idProduct}=="013*", MODE:="0666"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1fc9", ATTRS{idProduct}=="013*", MODE:="0666"
+  '';
+
   environment.pathsToLink = [
     "/share/nix-direnv"
   ];
