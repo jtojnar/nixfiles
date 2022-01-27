c893769de05d9a82ad29b5733e8c7ae6c8160e0c
Jan Tojnar <jtojnar@gmail.com>
2022-01-27 23:37:11 +0100

    flake: update
    Also revert the pkexec patch since it is now included.

--- common/profiles/core.nix
+++ common/profiles/core.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 {
   nix = {
     package = pkgs.nixUnstable;
@@ -16,17 +16,4 @@
   programs.command-not-found.enable = false;
 
   users.mutableUsers = false;
-
-  security.wrappers.pkexec.source =
-    let
-      freshPolkit = pkgs.polkit.overrideAttrs (attrs: {
-        patches = attrs.patches or [ ] ++ [
-          (pkgs.fetchpatch {
-            url = "https://gitlab.freedesktop.org/polkit/polkit/-/commit/a2bf5c9c83b6ae46cbd5c779d3055bff81ded683.patch";
-            sha256 = "162jkpg2myq0rb0s5k3nfr4pqwv9im13jf6vzj8p5l39nazg5i4s";
-          })
-        ];
-      });
-    in
-    lib.mkForce "${freshPolkit.bin}/bin/pkexec";
 }
