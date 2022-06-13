bbd93e5c84985086d8f322ea22486d19add3bdbe
Jan Tojnar <jtojnar@gmail.com>
2022-06-13 15:17:06 +0200

    pkgs.flarum: Update composer dependencies
    Also fix the update script

--- pkgs/flarum/update.sh
+++ pkgs/flarum/update.sh
@@ -6,20 +6,20 @@ set -euo pipefail
 owner=flarum
 repo=flarum
 latestVersion=$(curl "https://api.github.com/repos/$owner/$repo/releases/latest" | jq --raw-output '.tag_name | sub("^v"; "")')
-currentVersion=$(nix-instantiate --eval --json --expr 'with import ./. {}; flarum.version' | jq --raw-output '.')
+currentVersion=$(nix-instantiate --eval --json --expr 'with import ./. {}; flarum.flarum.version' | jq --raw-output '.')
 
 if [[ "$currentVersion" == "$latestVersion" && "${BUMP_LOCK-}" != "1" ]]; then
     # Skip update when already on the latest version.
     exit 0
 fi
 
-update-source-version flarum "$latestVersion"
+update-source-version flarum.flarum "$latestVersion"
 
 repoSelf="$(nix eval -f . --json 'outPath' | jq --raw-output '.')"
 # Flakes might place the file into store so we need to switch it back to source tree.
 # Assuming PWD is repository root.
 dirname="$(realpath --relative-to="$repoSelf" "$(dirname "$0")")"
-sourceDir="$(nix-build -A flarum.src --no-out-link)"
+sourceDir="$(nix-build -A flarum.flarum.src --no-out-link)"
 tempDir="$(mktemp -d)"
 
 pushd "$tempDir"
