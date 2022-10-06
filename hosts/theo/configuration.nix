e0c3377ede645473dd0898823b719b2e412e9a0c
Jan Tojnar <jtojnar@gmail.com>
2022-10-06 07:00:37 +0200

    theo: install wireshark-qt

--- hosts/theo/configuration.nix
+++ hosts/theo/configuration.nix
@@ -328,7 +328,10 @@ in {
       package = pkgs.gnomeExtensions.gsconnect;
     };
 
-    wireshark.enable = true;
+    wireshark = {
+      enable = true;
+      package = pkgs.wireshark;
+    };
   };
 
   documentation = {
