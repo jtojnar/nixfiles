257b252a934d5737d12270cdbab21a85f1f6c775
Jan Tojnar <jtojnar@gmail.com>
2022-10-15 21:54:54 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'agenix':
        'github:ryantm/agenix/6acb1fe5f8597d5ce63fc82bc7fcac7774b1cdf0' (2022-09-25)
      → 'github:ryantm/agenix/ff2dc4fb88e08258ece0a48f0f7a90bc0aadc514' (2022-10-15)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/e1f1160284198a68ea8c7fffbbb1436f99e46ef9' (2022-10-08)
      → 'github:nix-community/home-manager/2ecb3ea990cf737cfb42d8cd805fa86347c1afaf' (2022-10-15)
    • Updated input 'napalm':
        'github:nix-community/napalm/cfb58e79f2ef995387102debb084f2c2fb674085' (2022-09-15)
      → 'github:nix-community/napalm/f9b0ab8b71f8566d492450591f0422a9ec47710c' (2022-10-10)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/c5924154f000e6306030300592f4282949b2db6c' (2022-10-08)
      → 'github:nixos/nixpkgs/4428e23312933a196724da2df7ab78eb5e67a88e' (2022-10-14)
    
      Updates to GNOME 43.
    
      Includes debug info for GNOME Shell and Mutter.

--- common/overlays/debugging/default.nix
+++ common/overlays/debugging/default.nix
@@ -3,11 +3,5 @@ final: prev: {
     gnome-control-center = gprev.gnome-control-center.overrideAttrs (attrs: {
       separateDebugInfo = true;
     });
-    gnome-shell = gprev.gnome-shell.overrideAttrs (attrs: {
-      separateDebugInfo = true;
-    });
-    mutter = gprev.mutter.overrideAttrs (attrs: {
-      separateDebugInfo = true;
-    });
   });
 }
