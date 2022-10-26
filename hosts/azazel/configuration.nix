e788cbf3ca2ab59145972177c244595e0e5e42b8
Jan Tojnar <jtojnar@gmail.com>
2022-10-26 13:30:30 +0200

    Prepare for Postgres upgrade
    I took the config from NixOS manual:
    https://nixos.org/manual/nixos/unstable/index.html#module-postgresql
    
    But running the `upgrade-pg-cluster` on `azazel` was failing with:
    
        Checking for presence of required libraries                 fatal
    
        Your installation references loadable libraries that are missing from the
        new installation.  You can add these libraries to the new installation,
        or remove the functions using them from the old installation.  A list of
        problem libraries is in the file:
            loadable_libraries.txt
    
    The mentioned file contained:
    
        could not load library "$libdir/plv8-3.0.0": ERROR:  could not access file "$libdir/plv8-3.0.0": No such file or directory
        In database: pqe
    
    After the last time (https://github.com/plv8/plv8/issues/466), I knew to fix that using:
    
        sudo -u postgres psql pqe -c 'UPDATE pg_proc SET probin = \'$libdir/plv8-3.1.4\' WHERE proname LIKE \'plv8%\';'
        sudo -u postgres psql pqe -c 'ALTER EXTENSION plv8 UPDATE;'
    
    But even then, it was still failing with:
    
        could not load library "$libdir/plv8-3.1.4": ERROR:  could not access file "$libdir/plv8-3.1.4": No such file or directory
        In database: pqe
    
    Turns out I needed the `NEWBIN` to be aware of all the extensions. That got me much farther this time but it still failed:
    
        Copying old pg_xact to new server
        *failure*
    
        Consult the last few lines of "pg_upgrade_utility.log" for
        the probable cause of the failure.
    
    Aargh:
    
        pg_restore: error: could not execute query: ERROR:  function unaccent(character varying) does not exist
        LINE 2:     SELECT unaccent($1)
                           ^
        HINT:  No function matches the given name and argument types. You might need to add explicit type casts.
        QUERY:
            SELECT unaccent($1)
    
        CONTEXT:  SQL function "immutable_unaccent" during inlining
        Command was:
        -- For binary upgrade, must preserve pg_class oids
        SELECT pg_catalog.binary_upgrade_set_next_index_pg_class_oid('16643'::pg_catalog.oid);
    
        CREATE INDEX "normalized_name" ON "public"."member" USING "btree" ("lower"("public"."immutable_unaccent"(("firstname")::character varying)), "lower"("public"."immutable_unaccent"(("lastname")::character varying)), "country_code");
    
    To fix that I had to modify the function in `pqe` database to refer to `unaccent` function using the `public` schema:
    
        CREATE OR REPLACE FUNCTION public.immutable_unaccent(character varying) RETURNS text
            LANGUAGE sql IMMUTABLE
            AS $_$
            SELECT public.unaccent($1)
          $_$;
        ALTER FUNCTION public.immutable_unaccent(character varying) OWNER TO pqe;
    
    After that, the upgrade succeeded.
    
    I did the same on `theo`, but there replacing the `immutable_unaccent` was not necessary.

--- hosts/azazel/configuration.nix
+++ hosts/azazel/configuration.nix
@@ -40,6 +40,32 @@ in {
     ripgrep
     sd
     tldr
+
+    (let
+      newPostgres = pkgs.postgresql_14.withPackages (pp: [
+        pp.plv8
+      ]);
+    in pkgs.writeScriptBin "upgrade-pg-cluster" ''
+      set -eux
+      systemctl stop postgresql
+
+      export NEWDATA="/var/lib/postgresql/${newPostgres.psqlSchema}"
+
+      # XXX specify the postgresql package you'd like to upgrade to
+      export NEWBIN="${newPostgres}/bin"
+
+      export OLDDATA="${config.services.postgresql.dataDir}"
+      export OLDBIN="${config.services.postgresql.package}/bin"
+
+      install -d -m 0700 -o postgres -g postgres "$NEWDATA"
+      cd "$NEWDATA"
+      sudo -u postgres $NEWBIN/initdb -D "$NEWDATA"
+
+      sudo -u postgres $NEWBIN/pg_upgrade \
+        --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
+        --old-bindir $OLDBIN --new-bindir $NEWBIN \
+        "$@"
+    '')
   ];
 
   # Prefer using cached builds over saving space.
