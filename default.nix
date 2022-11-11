cbec8e51016771c1c67e59780b8b18e57192e8ce
Jan Tojnar <jtojnar@gmail.com>
2022-11-11 14:58:33 +0100

    nix: Support using nixpkgs-hammering through flake-compat

--- default.nix
+++ default.nix
@@ -1,5 +1,7 @@
-# A function so that this can be imported like nixpkgs by various update scripts.
+# A function so that this can be imported like nixpkgs by various update scripts and nixpkgs-hammering.
 {
+  # Required by nixpkgs-hammering.
+  overlays ? [ ],
   ...
 }:
 
@@ -14,7 +16,11 @@ let
   };
 
   packages = self.defaultNix.outputs.legacyPackages.${builtins.currentSystem};
+
+  # Our `legacyPackages` are Nixpkgs’s `legacyPackages` overlaid with our custom packages
+  # so we can just extend it further with the hammering overlays.
+  packagesWithExtraOverlays = packages.appendOverlays overlays;
 in
-# Prepend all packages for current system so that various update scripts can find the packages without having to recurse into outputs.
-packages
+# Prepend all packages for current system so that various update scripts and nixpkgs-hammering can find the packages without having to recurse into outputs.
+packagesWithExtraOverlays
 // self.defaultNix
