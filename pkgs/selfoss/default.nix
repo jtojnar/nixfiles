56f19ec8ec7ae45f5409d5740fcd2bba57e2ab54
Jan Tojnar <jtojnar@gmail.com>
2022-12-22 21:50:56 +0000

    selfoss: 2.20-2417f4a → 2.20-d45cba4

--- pkgs/selfoss/default.nix
+++ pkgs/selfoss/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   pname = "selfoss";
-  version = "2.20-2417f4a";
+  version = "2.20-d45cba4";
 
   src = fetchurl {
     url = "https://dl.cloudsmith.io/public/fossar/selfoss-git/raw/names/selfoss.zip/versions/${version}/selfoss-${version}.zip";
-    sha256 = "qu5SItyukvAceYg8x6yNWrHCSb6ugnGaM9xgdqgdSNY=";
+    sha256 = "O27HR3c8bDUn9rnAgd/aK+SvKAEB+Qs7enk/AkuvJMQ=";
   };
 
   nativeBuildInputs = [
