9439ece9e3407093baf2902843677fd7b84941ff
Jan Tojnar <jtojnar@gmail.com>
2023-04-24 15:31:00 +0200

    common: Move Sublime Keymap to a separate file
    This will give use syntax highlighting.

--- common/configs/sublime/default.nix
+++ common/configs/sublime/default.nix
@@ -8,21 +8,5 @@
     hash = "sha256-P2yU9hVq8epaa7F0gzFg6uzzZCLg3qskwN/qYjhwR64=";
   };
 
-  home.file.".config/sublime-text/Packages/User/Default.sublime-keymap".text = ''
-    [
-      {
-        "keys": ["shift+escape"],
-        "command": "single_last_selection",
-        "context": [
-          { "key": "num_selections", "operator": "not_equal", "operand": 1 }
-        ]
-      },
-
-      // Use Ctrl-Tab for switching tabs as they appear in the tab bar rather than LIFO.
-      { "keys": ["ctrl+tab"], "command": "next_view" },
-      { "keys": ["ctrl+shift+tab"], "command": "prev_view" },
-      { "keys": ["ctrl+pagedown"], "command": "next_view_in_stack" },
-      { "keys": ["ctrl+pageup"], "command": "prev_view_in_stack" },
-    ]
-  '';
+  home.file.".config/sublime-text/Packages/User/Default.sublime-keymap".source = ./Default.sublime-keymap;
 }
