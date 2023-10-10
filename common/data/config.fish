ba3d61ad67ec3fa66f5764a92a1f7281e6398506
Jan Tojnar <tojnar@ttc.cz>
2023-10-10 20:16:37 +0200

    sy: Drop unused environment variables
    - QT_QPA_PLATFORMTHEME: Maybe this was what was causing Qt rendering issues
    - ATOM_HOME: Atom is dead.
    - CCACHE_DIR: Never really used ccache.
    - GTK2_RC_FILES: Hopefully, we will no longer need to configure or use GTK 2.
    - RUSTUP_HOME: Using Nix for now.
    - STACK_ROOT: Using Nix for now.
    - WEECHAT_HOME: Never really used it.
    - XCOMPOSEFILE: Since 0fc9c50079b353e5f1204d30661ddf95f97b4b3c installed by home-manager.
    - PATH: Imperative software installation is yuck.

--- common/data/config.fish
+++ common/data/config.fish
@@ -3,26 +3,15 @@ set fish_greeting
 set -x EDITOR nano
 set -x TERMINAL gnome-terminal
 
-set -x QT_QPA_PLATFORMTHEME qt5ct
-
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_CACHE_HOME $HOME/.cache
 
-set -x ATOM_HOME $XDG_DATA_HOME/atom
 set -x CABAL_CONFIG $XDG_DATA_HOME/cabal/config
 set -x CARGO_HOME $XDG_DATA_HOME/cargo
-set -x CCACHE_DIR $XDG_CACHE_HOME/ccache
 set -x GNUPGHOME $XDG_DATA_HOME/gnupg
-set -x GTK2_RC_FILES $XDG_CONFIG_HOME/gtk-2.0/gtkrc
 set -x NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
-set -x RUSTUP_HOME $XDG_DATA_HOME/rustup
-set -x STACK_ROOT $XDG_DATA_HOME/stack
-set -x WEECHAT_HOME $XDG_CONFIG_HOME/weechat
-set -x XCOMPOSEFILE $XDG_CONFIG_HOME/x11/xcompose
 set -x RIPGREP_CONFIG_PATH $XDG_CONFIG_HOME/ripgrep/config
-
-set PATH $PATH $CARGO_HOME/bin $HOME/.local/bin
 
 alias clip "xsel --clipboard"
 alias reset "tput reset"
