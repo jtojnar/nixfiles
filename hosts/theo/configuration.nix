6471155f06cb2ad9bae6a5af173d3e7cb8a39a6a
Jan Tojnar <jtojnar@gmail.com>
2023-12-16 22:29:02 +0100

    Fix gh migration
    This was broken by https://github.com/NixOS/nixpkgs/commit/89d4c2fc4b3c896ee5ccf32a8441e8bf89590b1b
    which was part of d4229aed4be61bf4f0de3d9877200b8638cc957e

--- hosts/theo/configuration.nix
+++ hosts/theo/configuration.nix
@@ -628,6 +628,9 @@ in {
     programs.gh = {
       enable = true;
       settings = {
+        # Workaround for https://github.com/nix-community/home-manager/issues/4744
+        version = 1;
+
         git_protocol = "ssh";
         editor = "subl -w";
         aliases = {
