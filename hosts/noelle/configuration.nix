jtojnar/nixfiles

commit f0938b6cb8ed7422d2eac562e43d1e012dd68786

Move ST config into a profile

hosts/noelle/configuration.nix
@@ -55,7 +55,7 @@ in {
 
   home-manager.users.dtojnaro = { lib, ... }: {
     imports = [
-      ../../common/configs/sublime
+      inputs.self.homeModules.profiles.sublime
     ];
 
     dconf.settings = {

hosts/sy/configuration.nix
@@ -165,8 +165,8 @@ in
 
   home-manager.users.jtojnar = { lib, ... }: {
     imports = [
-      ../../common/configs/sublime
       inputs.self.homeModules.profiles.ripgrep
+      inputs.self.homeModules.profiles.sublime
     ];
 
     dconf.settings = {

hosts/theo/configuration.nix
@@ -488,8 +488,8 @@ in {
   home-manager.users.jtojnar = { lib, ... }: {
     imports = [
       ../../common/configs/keepassxc
-      ../../common/configs/sublime
       inputs.self.homeModules.profiles.ripgrep
+      inputs.self.homeModules.profiles.sublime
     ];
 
     dconf.settings = {

common/home-profiles/list.nix
@@ -1,3 +1,4 @@
 [
   ./ripgrep.nix
+  ./sublime/default.nix
 ]

commit 74bd484fd8bc3f5e6441bb8c4b9821be8e69b278

noelle: fix evaluation
This got broken in f0938b6cb8ed7422d2eac562e43d1e012dd68786

hosts/noelle/configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, inputs, pkgs, ... }:
 
 let
   keys = import ../../common/data/keys.nix;