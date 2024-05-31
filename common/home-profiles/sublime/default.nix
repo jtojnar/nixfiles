316042a8e29f34a1ca03f07acd3ba6008e703d1d
Jan Tojnar <jtojnar@gmail.com>
2024-05-31 13:46:15 +0200

    common: Install Sublime settings as a directory
    This will be more convenient than listing each file individually.
    
    Using `Declaratives` to avoid conflict with the previous `Declarative` directory.

--- common/home-profiles/sublime/default.nix
+++ common/home-profiles/sublime/default.nix
@@ -8,10 +8,5 @@
     hash = "sha256-P2yU9hVq8epaa7F0gzFg6uzzZCLg3qskwN/qYjhwR64=";
   };
 
-  home.file.".config/sublime-text/Packages/User/Default.sublime-keymap".source = ./Default.sublime-keymap;
-
-  home.file.".config/sublime-text/Packages/Declarative/Preferences.sublime-settings".source = ./Preferences.sublime-settings;
-
-  home.file.".config/sublime-text/Packages/Declarative/delete_subword_forward.sublime-macro".source = ./delete_subword_forward.sublime-macro;
-  home.file.".config/sublime-text/Packages/Declarative/delete_subword.sublime-macro".source = ./delete_subword.sublime-macro;
+  home.file.".config/sublime-text/Packages/Declaratives".source = ./Declarative;
 }
