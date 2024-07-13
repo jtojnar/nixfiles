f3af505fd87d9942a22f508a66246a9dafef3601
Jan Tojnar <jtojnar@gmail.com>
2024-07-13 03:25:55 +0200

    pengu: Switch to buildNpmPackage
    napalm is basically dead nowadays, and we can use update scripts for bumping deps hash.
    This will also make evaluation slightly faster by moving the dependency fetching to single FOD (at the cost of reuse).
    `buildNpmPackage` is also significantly more ergonomic.

--- pkgs/pengu/default.nix
+++ pkgs/pengu/default.nix
@@ -1,75 +1,32 @@
 {
-  runCommand,
+  lib,
   fetchFromGitHub,
-  napalm,
+  buildNpmPackage,
   nodejs,
-  python3,
   unstableGitUpdater,
+  _experimental-update-script-combinators,
+  common-updater-scripts,
 }:
 
-let
-  stopNpmCallingHome = ''
-    # Do not try to find npm in napalm-registry –
-    # it is not there and checking will slow down the build.
-    npm config set update-notifier false
-
-    # Same for security auditing, it does not make sense in the sandbox.
-    npm config set audit false
-  '';
+buildNpmPackage {
+  pname = "pengu";
+  version = "0-unstable-2023-12-20";
 
   src = fetchFromGitHub {
     owner = "jtojnar";
     repo = "pengu";
     rev = "29dcc190c0250b6f5268000449cddc54ca65f597";
-    sha256 = "sha256-Osc9/tDsONdCm8U9HSgSxJr74WYeH8t72Ql//CzIb+M=";
-  };
-in
-napalm.buildPackage src rec {
-  pname = "pengu";
-  version = "unstable-2023-12-20";
-
-  customPatchPackages = {
-    # Patch shebangs.
-    "node-gyp-build" = pkgs: prev: {
-    };
-    "node-gyp-build-optional-packages" = pkgs: prev: {
-    };
+    hash = "sha256-Osc9/tDsONdCm8U9HSgSxJr74WYeH8t72Ql//CzIb+M=";
   };
 
-  nativeBuildInputs = [
-    # For node-gyp
-    python3
-  ];
+  npmDepsHash = "sha256-NLjWPwinqtClWo4z/O5osUpcqlpMPlGv7f3jiyqyTzM=";
 
-  inherit nodejs;
+  postInstall = ''
+    # Move stuff back to root directory for ease of use.
+    mv "$out/lib/node_modules/pengu/"* "$out"
+    rmdir "$out/lib/node_modules"{/pengu,}
 
-  npmCommands = [
-    # Let’s install again, this time running scripts.
-    "npm install --loglevel verbose --nodedir=${nodejs}/include/node"
-
-    # Patch shebangs so that scripts can run.
-    # napalm’s patching is not “overzealous” enough
-    # to fix the file linked by “node_modules/.bin/parcel”.
-    "patchShebangs node_modules/parcel/lib/bin.js"
-
-    # Build the front-end.
-    "npm run build"
-
-    # Clean up node_modules for production.
-    "npm install --only=production --loglevel verbose"
-  ];
-
-  postConfigure = ''
-    # configurePhase sets $HOME
-    ${stopNpmCallingHome}
-  '';
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p "$out"
-    cp -r * "$out"
-
+    # Install systemd service.
     mkdir -p "$out/lib/systemd/system"
     echo > "$out/lib/systemd/system/pengu.service" "
     [Unit]
@@ -83,15 +40,20 @@ napalm.buildPackage src rec {
     RestartSec=10
     WorkingDirectory=$out
     "
-
-    runHook postInstall
   '';
 
-  passthru = {
-    inherit src;
-    updateScript = unstableGitUpdater {
-      # The updater tries src.url by default, which does not exist for fetchFromGitHub (fetchurl).
-      url = "${src.meta.homepage}.git";
-    };
-  };
+  passthru.updateScript =
+    let
+      updateSource = unstableGitUpdater { };
+      updateDeps = [
+        (lib.getExe' common-updater-scripts "update-source-version")
+        "pengu"
+        "--ignore-same-version"
+        "--source-key=npmDeps"
+      ];
+    in
+    _experimental-update-script-combinators.sequence [
+      updateSource
+      updateDeps
+    ];
 }
