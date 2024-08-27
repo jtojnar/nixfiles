67caa6cb22b4dd57e6ad12dbd491b4d8cdb63159
Jan Tojnar <jtojnar@gmail.com>
2024-08-27 13:27:01 +0200

    common: Remove GNOME Control Center debugging overlay
    It provides debug info out of the box since https://github.com/NixOS/nixpkgs/commit/a78861db45f194b3d829233602dc124c1c257e0d

--- common/overlays/debugging/default.nix
+++ common/overlays/debugging/default.nix
@@ -1,7 +1,2 @@
 final: prev: {
-  gnome = prev.gnome.overrideScope (gfinal: gprev: {
-    gnome-control-center = gprev.gnome-control-center.overrideAttrs (attrs: {
-      separateDebugInfo = true;
-    });
-  });
 }
