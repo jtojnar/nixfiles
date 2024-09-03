6efc2b9d51ec0f9b5c2f5f11ee5606ce41735d01
Jan Tojnar <jtojnar@gmail.com>
2024-09-03 09:23:39 +0200

    common/postgres: Add upgrade infrastructure
    This will make upgrading database much easier. Just rebuild with something like:
    
        custom.postgresql.upgradeTargetPackage = pkgs.postgresql_16 (pp: [
          pp.plv8
        ]);
    
    Then follow the instructions in the ordered list from the NixOS manual:
    https://nixos.org/manual/nixos/unstable/#module-services-postgres-upgrading

--- common/modules/postgres.nix
+++ common/modules/postgres.nix
@@ -36,6 +36,12 @@ in
         default = [];
         description = "List of databases to set up.";
       };
+
+      upgradeTargetPackage = mkOption {
+        type = types.nullOr types.package;
+        default = null;
+        description = "PostgreSQL package that we want to upgrade to. When set, an update script will be installed.";
+      };
     };
   };
 
@@ -109,6 +115,31 @@ in
         };
       };
     };
+
+    # https://nixos.org/manual/nixos/unstable/#module-services-postgres-upgrading
+    environment.systemPackages = lib.mkIf (cfg.upgradeTargetPackage != null) [
+      (pkgs.writeScriptBin "upgrade-pg-cluster" ''
+        set -eux
+        # XXX it's perhaps advisable to stop all services that depend on postgresql
+        systemctl stop postgresql
+
+        export NEWDATA="/var/lib/postgresql/${cfg.upgradeTargetPackage.psqlSchema}"
+
+        export NEWBIN="${cfg.upgradeTargetPackage}/bin"
+
+        export OLDDATA="${config.services.postgresql.dataDir}"
+        export OLDBIN="${config.services.postgresql.package}/bin"
+
+        install -d -m 0700 -o postgres -g postgres "$NEWDATA"
+        cd "$NEWDATA"
+        sudo -u postgres $NEWBIN/initdb -D "$NEWDATA"
+
+        sudo -u postgres $NEWBIN/pg_upgrade \
+          --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
+          --old-bindir $OLDBIN --new-bindir $NEWBIN \
+          "$@"
+      '')
+    ];
   };
 
 }
