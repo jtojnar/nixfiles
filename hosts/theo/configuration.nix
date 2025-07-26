jtojnar/nixfiles

commit efa342601bec296cdb90a3d71b9ae4cea1b85c43

flake.lock: Update
Flake lock file updates:

• Updated input 'home-manager':
    'github:nix-community/home-manager/02077149e2921014511dac2729ae6dadb4ec50e2' (2025-05-28)
  → 'github:nix-community/home-manager/c7fdb7e90bff1a51b79c1eed458fb39e6649a82a' (2025-06-02)
• Updated input 'nixpkgs':
    'github:nixos/nixpkgs/4faa5f5321320e49a78ae7848582f684d64783e9' (2025-05-27)
  → 'github:nixos/nixpkgs/910796cabe436259a29a72e8d3f5e180fc6dfacc' (2025-05-31)

  GNOME module moved out of x11.
• Updated input 'vpsadminos':
    'github:vpsfreecz/vpsadminos/35243660ffabb25b65258c06360b97aa3a8fab5a' (2025-05-28)
  → 'github:vpsfreecz/vpsadminos/27b7693f13544b2aeb6bcb9b413221d62c97ceb7' (2025-05-30)

hosts/theo/configuration.nix
@@ -349,8 +349,9 @@ in
   };
 
   # Enable the Desktop Environment.
-  services.xserver = {
-    enable = true;
+  services.xserver.enable = true;
+
+  services = {
     displayManager = {
       gdm = {
         enable = true;

hosts/evan/configuration.nix
@@ -69,8 +69,8 @@ in
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   home-manager.users.michal =
     { lib, ... }:

hosts/noelle/configuration.nix
@@ -56,8 +56,8 @@ in
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   home-manager.users.dtojnaro =
     { lib, ... }:

hosts/sy/configuration.nix
@@ -76,11 +76,13 @@ in
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  services.xserver = {
+  services = {
     # Enable the GNOME Desktop Environment.
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
+  };
 
+  services.xserver = {
     # Configure keymap in X11
     xkb = {
       layout = "cz";

commit 5500255726b2086db4a3a6f3ad32df3317312100

theo: Do not default to Xorg
Wayland seems to work okay now.

This also fixes “error: attribute 'displayManager' already defined” during evaluation with Nix < 2.26.0, which got broken in efa342601bec296cdb90a3d71b9ae4cea1b85c43.

hosts/theo/configuration.nix
@@ -344,10 +344,6 @@ in
   programs.gnome-terminal.enable = true;
   programs.gpaste.enable = true;
 
-  services.displayManager = {
-    defaultSession = "gnome-xorg";
-  };
-
   # Enable the Desktop Environment.
   services.xserver.enable = true;
 