1721b04ee810253ffb280143e4c851011fd0822f
Jan Tojnar <jtojnar@gmail.com>
2025-10-13 17:34:45 +0200

    fish: Restore previous navigation key bindings
    Fish 4.1.0 changed them. I prefer the old ones which better match my text editor.
    
    https://fishshell.com/docs/current/relnotes.html#new-or-improved-bindings
    https://github.com/fish-shell/fish-shell/commit/2bb5cbc95943b0168c8ceb5b639391299e767e72
    https://github.com/fish-shell/fish-shell/issues/10926#issuecomment-3395993047
    
    Weirdly, binding `ctrl-backspace` does not work as according to `fish_key_reader -c -V`,
    when I press `ctrl-backspace`, GNOME Terminal sends `\x08`, the same as for `ctrl-h`.
    https://github.com/fish-shell/fish-shell/issues/11538

--- common/data/config.fish
+++ common/data/config.fish
@@ -3,3 +3,16 @@ set fish_greeting
 alias clip "xsel --clipboard"
 alias reset "tput reset"
 alias diff-cleaner "filterdiff --strip=0 --clean"
+
+# Restore previous key bindings
+# https://github.com/fish-shell/fish-shell/issues/10926
+bind alt-left prevd-or-backward-word
+bind alt-right nextd-or-forward-word
+bind alt-backspace backward-kill-word
+bind alt-delete kill-word
+bind ctrl-left backward-token
+bind ctrl-right forward-token
+bind ctrl-backspace backward-kill-token
+# fish actually thinks I press ctrl-h when I press ctrl-backpace
+bind ctrl-h backward-kill-token
+bind ctrl-delete kill-token
