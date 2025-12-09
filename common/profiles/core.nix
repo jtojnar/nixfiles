29826e3454c8160b187219e78d068ee597ed29fd
Jan Tojnar <jtojnar@gmail.com>
2025-12-09 01:28:19 +0100

    common/profiles/core: Use file-roller for all archives
    Nautilus is annoying, automatically extracting the archive in place.
    That is slow, especially when most of the time, I just want to look at a single file.
    
    Unfortunately, we cannot just replace it with `environment.systemPackages` because `gnome-session` adds its own `share` directory to `XDG_DATA_DIRS` so it would take precedence:
    
    https://specifications.freedesktop.org/mime-apps/latest/file.html

--- common/profiles/core.nix
+++ common/profiles/core.nix
@@ -18,4 +18,15 @@
   programs.command-not-found.enable = false;
 
   users.mutableUsers = false;
+
+  environment.etc."xdg/gnome-mimeapps.list".source =
+    pkgs.runCommand "gnome-mimeapps-overridden.list" { }
+      ''
+        cp "${pkgs.gnome-session}/share/applications/gnome-mimeapps.list" "$out"
+
+        # Use File Roller for all archives
+        substituteInPlace "$out" \
+          --replace-fail "org.gnome.Nautilus.desktop" 'org.gnome.FileRoller.desktop' \
+          --replace-fail "inode/directory=org.gnome.FileRoller.desktop" 'inode/directory=org.gnome.Nautilus.desktop'
+      '';
 }
